8f93c1e9eb866a6b725bf4ac2876674a30b813fa
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-07-11 22:54:24 -0500

    Toggle Info

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -8,8 +8,11 @@ const SingleQuestion = ({ title, info }) => {
     <article className="question">
       <header>
         <h5>{title}</h5>
+        <button className="question-btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
       </header>
-      <p>{info}</p>
+      {showInfo && <p>{info}</p>}
     </article>
   );
 };
