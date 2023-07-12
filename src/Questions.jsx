b1da0f2a89fd99499f68618b1f6622b9ae204890
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-07-11 22:29:10 -0500

    Render List

---
+++ src/Questions.jsx
@@ -0,0 +1,14 @@
+import SingleQuestion from "./SingleQuestion";
+
+const Questions = ({ questions }) => {
+  return (
+    <section className="container">
+      <h1>questions</h1>
+      {questions.map((question) => {
+        return <SingleQuestion key={question.id} {...question} />;
+      })}
+    </section>
+  );
+};
+
+export default Questions;
